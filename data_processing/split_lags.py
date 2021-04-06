33004b33b81828717a8c5e2a912a18dbcd96aa95
kzlecha <kzlecha@gmail.com>
2021-04-05 22:17:36 -0700

    add data processing for each file

--- data_processing/split_lags.py
+++ data_processing/split_lags.py
@@ -1,35 +1,42 @@
 from os import listdir
 from os.path import isfile, join
 
-from pandas import concat, read_csv, DataFrame
+from pandas import concat, read_csv
 
 
-def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
-    # n_vars = 1 if type(data) is list else data.shape[1]
-    # df = DataFrame(data)
-    cols = list()
-    # input sequence (t-n, ... t-1)
-    for i in range(n_in, 0, -1):
-        cols.append(df.shift(i))
-    # forecast sequence (t, t+1, ... t+n)
-    for i in range(0, n_out):
-        cols.append(df.shift(-i))
-    # put it all together
-    agg = concat(cols, axis=1)
-    # drop rows with NaN values
-    if dropnan:
-        agg.dropna(inplace=True)
-    return agg
+def reformat(dfPre):
+    """
+    reformat the dataframe
+    FIXME: explain in more detial
+    @param dfPre: dataframe before preprocessing
+    """
+    dfPre['priceUsd'] = dfPre['priceUsd'].fillna(method='pad')
+    df = dfPre.copy(True)
+
+    df['priceUsd'] = df.priceUsd.shift(-1)
+    df['priceBtc'] = df.priceBtc.shift(-1)
+
+    df = df.iloc[1:]
+    dfPre = dfPre.iloc[1:]
+
+    df["priceDirecetion"] = "neutral"
+
+    pos_mask = df['priceUsd'] >= dfPre['priceUsd'] * 1.001
+    neg_mask = df['priceUsd'] <= dfPre['priceUsd'] * 0.999
+    df.loc[pos_mask, "priceDirection"] = "positive"
+    df.loc[neg_mask, "priceDirection"] = "negative"
+
+    return df
+
 
 dir_path = "dataMar14/"
 file_list = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
 
-
 for filename in file_list:
 
     filepath = dir_path + filename
-    df = read_csv(filepath)
+    data = read_csv(filepath)
 
-    df = series_to_supervised(df, 6, 2)
+    data = reformat(data)
     
-    df.to_csv("data/"+filename, index=False)
+    data.to_csv("data/"+filename, index=False)
