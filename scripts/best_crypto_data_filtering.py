d3fec0dc64631bad078279c1ed3563a49f577fd9
kzlecha <kzlecha@gmail.com>
2021-05-06 15:23:50 -0700

    CHANGE - add previous times
    - add previous 12 price lags
    - update comparision to use new data

--- scripts/best_crypto_data_filtering.py
+++ scripts/best_crypto_data_filtering.py
@@ -10,11 +10,29 @@ def shift_sentiments(df, look_back=12):
     add the previous look_back sentiments to the dataframe
     """
     sentiments = {}
-    for column in [column for column in df.columns if column != "priceUsd"]:
+    for column in ["sentiment_positive_total", "sentiment_negative_total", "social_volume_total"]:
         for i in range(1, look_back+1, 1):
             sentiments[column+"_t-"+str(i)] = df[column].shift(i)
+    # print(sentiments.keys())
 
-    return DataFrame(sentiments)
+    return DataFrame(sentiments, index=df.index)
+
+
+def shift_price(df, look_back=12):
+    """
+    add the previous look_back sentiments to the dataframe
+    """
+    price_data = get_price_data(df)
+    # print(price_data.head())
+
+    historical_price = {}
+    for column in price_data.columns:
+        for i in range(1, look_back+1, 1):
+            historical_price[column+"_t-"+str(i)] = price_data[column].shift(i)
+
+    # print( len(DataFrame(historical_price)))
+
+    return DataFrame(historical_price, index=df.index)
 
 
 def get_price_data(df, look_back=12):
@@ -29,7 +47,11 @@ def get_price_data(df, look_back=12):
 
     mean = price_list.mean(axis=1)
     std = price_list.std(axis=1)
-    return mean, std
+
+    price_data = DataFrame()
+    price_data["mean_price"], price_data["std_price"] = mean, std
+
+    return price_data
 
 
 def assign_best_price(df):
@@ -70,22 +92,28 @@ def reformat(dfPre):
     ---
     @param dfPre: dataframe before preprocessing
     """
-    prices = dfPre['priceUsd'].fillna(method='pad')
     dates = dfPre["datetime"]
+    dfPre.index = dates
+
+    prices = dfPre['priceUsd'].fillna(method='pad')
 
     exclude_cols = [0,1,2,4,5,6,7,9,11,12,13,14,16,18,19,20,21,23,25]
-    dfPre = dfPre.iloc[:, ~dfPre.columns.isin(dfPre.columns[exclude_cols])]
+    # dfPre = dfPre.iloc[:, ~dfPre.columns.isin(dfPre.columns[exclude_cols])]
 
     # shift the sentiments
-    # df = shift_sentiments(dfPre)
-    df = dfPre.copy(deep=True)
+    # df_sentiments = shift_sentiments(dfPre)
+    df_sentiments = dfPre[["sentiment_positive_total", "sentiment_negative_total", "social_volume_total"]]
+    # df_sentiments = DataFrame(index=dfPre.index)
 
     # get the new prices
-    df["mean_price_before"], df["std_price_before"] = get_price_data(dfPre)
-    df["priceUsd"] = prices
-    df.index = dates
+    df_prices = shift_price(dfPre)
 
-    df["price_diff"] = df['priceUsd'] - df['mean_price_before']
+
+    # return
+    df = concat([df_sentiments, df_prices], axis=1)
+    df["priceUsd"] = prices
+
+    df["price_diff"] = df['priceUsd'] - df['mean_price_t-1']
 
     return df.dropna(axis=0)
 
@@ -111,7 +139,7 @@ new_df = assign_best_price(new_df)
 
 new_df = new_df.loc[new_df["best_crypto"] != ""]
 
-new_df.to_csv("dataLSTM/combined_cryptos/historical_sentiment_2h.csv", index=False)
+new_df.to_csv("dataLSTM/combined_cryptos/price_2h.csv", index=False)
 
 # print(new_df.groupby("best_crypto")["best_crypto"].count())
-print(new_df.columns)
+# print(new_df.columns)
